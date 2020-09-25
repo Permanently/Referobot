babf2f420c69d95e6f5a112bd572b74e8577489c
Permanently <me@permanently.xyz>
2020-09-25 18:32:45 +0100

    Changed set channel method

--- src/commands/set.js
+++ src/commands/set.js
@@ -1,5 +1,7 @@
 import Discord from 'discord.js';
-import { checkAdmin } from '../checkPerms.js';
+import {
+  checkAdmin
+} from '../checkPerms.js';
 
 export let channelPerm = undefined;
 
@@ -9,21 +11,32 @@ const set = (msg, cmd, subcmd) => {
     channelPerm = undefined;
     msg.channel.send({
       embed: richEmbed
-              .setColor('#ffffff')
-              .setDescription(`Referral checking can now be done in any channel.`)
+        .setColor('#ffffff')
+        .setDescription(`Referral checking can now be done in any channel.`)
     })
   } else if (checkAdmin(msg) === true) {
-    channelPerm = subcmd;
-    msg.channel.send({
-      embed: richEmbed
-              .setColor('#ffffff')
-              .setDescription(`Referral checking channel is now ${channelPerm}.`)
-    })
+    channelPerm = subcmd.substring(2).substring(0,18);
+
+    if (member.guild.channels.find(channel => channel.id === channelPerm)) {
+      msg.channel.send({
+          embed: richEmbed
+            .setColor('#ffffff')
+            .setDescription(`You did not specify a valid channel.`)
+        }
+      )
+
+    } else {
+      msg.channel.send({
+          embed: richEmbed
+            .setColor('#ffffff')
+            .setDescription(`Referral checking channel is now ${channelPerm}.`)
+        }
+      )}
   } else {
     msg.channel.send({
       embed: richEmbed
-              .setColor('#ffffff')
-              .setDescription(`You don't have permission to do this.`)
+        .setColor('#ffffff')
+        .setDescription(`You don't have permission to do this.`)
     })
   }
 }
